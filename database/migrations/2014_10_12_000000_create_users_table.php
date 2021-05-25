01c61c40481d99453e9091d1f255dbc0ea577c85
Arnold Kunihira <arnold.kmulera@gmail.com>
2021-05-25 12:58:53 +0300

    Modified User model functionality to depict the User migration table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,31 +6,32 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-			$table->enum('role', ['admin', 'healthworker'])->default('admin');
+            $table->string('first_name', 55);
+            $table->string('middle_name', 55)->nullable();
+            $table->string('last_name', 55);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-			$table->string('api_token');
+            $table->string('nationality', 55);
+            $table->date('date_of_birth');
+            $table->string('user_mobile', 12);
+            $table->string('next_of_kin', 55);
+            $table->string('next_of_kin_mobile', 12);
+            $table->string('created_with', 5)->nullable();
+            $table->string('updated_with', 5)->nullable();
             $table->rememberToken();
             $table->timestamps();
+
+            $table->index('first_name');
+            $table->index('last_name');
+            $table->index('user_mobile');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('users');
