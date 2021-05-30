5f0bfc85be223cc0de246f16c22999edd7bca484
Arnold Kunihira <arnold.kmulera@gmail.com>
2021-05-30 18:26:07 +0300

    Add subject model and its table structure

---
+++ app/Subject.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Subject extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'first_name',
+        'middle_name',
+        'last_name',
+        'email',
+        'nationality',
+        'date_of_birth',
+        'phone',
+        'next_of_kin',
+        'next_of_kin_phone',
+        'id_number',
+        'id_type',
+    ];
+}
