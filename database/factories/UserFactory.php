4938e77f78f6c3bfd3dbb486df67a47993284736
Arnold Kunihira <arnold.kmulera@gmail.com>
2021-06-02 01:08:09 +0300

    Modify user factory class to suit version 8 database factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,35 +1,40 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+namespace Database\Factories;
 
 use App\User;
-use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
-*/
+class UserFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = User::class;
 
-$factory->define(User::class, function (Faker $faker) {
-    return [
-        'first_name' => $faker->name,
-        'middle_name' => $faker->name,
-        'last_name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
-        'nationality' => $faker->country,
-        'date_of_birth' => $faker->date,
-        'user_mobile' => $faker->phoneNumber,
-        'next_of_kin' => $faker->name,
-        'next_of_kin_mobile' => $faker->phoneNumber,
-    ];
-});
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'first_name' => $this->faker->name(),
+            'middle_name' => $this->faker->name(),
+            'last_name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+            'nationality' => $this->faker->country,
+            'date_of_birth' => $this->faker->date,
+            'user_mobile' => $this->faker->phoneNumber,
+            'next_of_kin' => $this->faker->name(),
+            'next_of_kin_mobile' => $this->faker->phoneNumber,
+        ];
+    }
+}
