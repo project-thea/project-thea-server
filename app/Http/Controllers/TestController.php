dd9bc968e3ca99a9175838167c70c6a713a47296
Arnold Kunihira <arnold.kmulera@gmail.com>
2021-06-03 14:22:42 +0300

    Modify api resources for the test controller

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,32 +5,34 @@ namespace App\Http\Controllers;
 use App\Helpers\APIHelpers;
 use App\Http\Resources\TestResource;
 use App\Test;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 class TestController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the Test.
      *
      * @return \Illuminate\Http\Response
      */
+
+    public const RECORDS_PER_PAGE = 20;
+
     public function index()
     {
-        $test = Test::select('tests.*', 'subjects.first_name', 'subjects.last_name', 'diseases.name')
+        $tests = Test::select('tests.*', 'subjects.first_name', 'subjects.last_name', 'diseases.name')
             ->leftJoin('subjects', 'tests.subject_id', '=', 'subjects.id')
             ->leftJoin('diseases', 'tests.disease_id', '=', 'diseases.id')
             ->orderBy('tests.test_date', 'ASC')
-            ->paginate(3);
+            ->paginate(self::RECORDS_PER_PAGE);
 
-        $testsCollection = TestResource::collection($test);
+        $testsCollection = TestResource::collection($tests);
         $apiResponse = APIHelpers::createAPIResponse(false, 'Tests retrieved successfully', $testsCollection);
         return response()->json($apiResponse, 200);
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Show the form for creating a new Test.
      *
      * @return \Illuminate\Http\Response
      */
@@ -40,7 +42,7 @@ class TestController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Store a newly created Test in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
@@ -50,9 +52,13 @@ class TestController extends Controller
         $data = $request->all();
 
         $validateData = [
-            'test_date' => 'required',
-            'status' => 'required|max:10',
-            'status_update_date' => 'required',
+            'disease_id' => 'exists:App\Disease,id',
+            'subject_id' => 'exists:App\Subject,id',
+            'test_date' => 'required|date',
+            'status' => 'required|string|max:10',
+            'status_update_date' => 'date',
+            'created_with' => 'string|max:4',
+            'updated_with' => 'string|max:4',
         ];
 
         $validator = Validator::make($data, $validateData);
@@ -62,24 +68,26 @@ class TestController extends Controller
         }
 
         $tests = Test::create($data);
-        $testResource = new TestResource($tests);
-        $apiResponse = APIHelpers::createAPIResponse(false, 'Test created successfully', $testResource);
+        $testsResource = new TestResource($tests);
+        $apiResponse = APIHelpers::createAPIResponse(false, 'Test created successfully', $testsResource);
         return response()->json($apiResponse, 201);
     }
 
     /**
-     * Display the specified resource.
+     * Display the specified Test.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Test $test)
     {
-        //
+        $testResource = new TestResource($test);
+        $apiResponse = APIHelpers::createAPIResponse(false, 'Test retrieved successfully', $testResource);
+        return response()->json($apiResponse, 200);
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * Show the form for editing the specified Test.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
@@ -90,7 +98,7 @@ class TestController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
+     * Update the specified Test in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Test  $test
@@ -111,7 +119,7 @@ class TestController extends Controller
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Remove the specified Test from storage.
      *
      * @param  \App\Test  $test
      * @return \Illuminate\Http\Response
@@ -120,6 +128,6 @@ class TestController extends Controller
     {
         $test->delete();
         $apiResponse = APIHelpers::createAPIResponse(false, 'Test deleted successfully', null);
-        return response()->json($apiResponse, 200);
+        return response()->json($apiResponse, 204);
     }
 }
