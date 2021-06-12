cfd0f00a3158a3f40237eee1b49118d56c6cae11
Arnold Kunihira <arnold.kmulera@gmail.com>
2021-06-12 18:06:53 +0300

    Add database factories used for checkup test feature tests

---
+++ database/factories/TestFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Test;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TestFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Test::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'disease_id' => function () {
+                return Disease::factory()->create()->id;
+            },
+            'subject_id' => function () {
+                return Subject::factory()->create()->id;
+            },
+            'test_date' => $this->faker->date(),
+            'status' => 'positive',
+            'status_update_date' => $this->faker->date(),
+            'created_with' => 'user',
+            'updated_with' => 'app'
+        ];
+    }
+}
