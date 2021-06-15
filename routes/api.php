b0a15c104537d0ab7522de189c2f67a6b24e4ea2
Arnold Kunihira <arnold.kmulera@gmail.com>
2021-06-15 13:38:52 +0300

    Modify web routes and configure jetstream inertia files for the web

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
 //Public routes
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
