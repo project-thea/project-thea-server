aed0f594db7c0058e4ce898ea694bf19b657b525
Arnold Kunihira <arnold.kmulera@gmail.com>
2021-06-21 02:22:33 +0300

    Modify test controller for test requests

--- app/Http/Controllers/Web/TestController.php
+++ app/Http/Controllers/Web/TestController.php
@@ -1,34 +1,35 @@
 <?php
 
-namespace App\Http\Controllers\Web;
+namespace App\Http\Controllers\WEB;
 
 use App\Http\Controllers\Controller;
-use App\Models\Subject;
 use App\Models\Test;
-use App\Models\Disease;
-use App\Models\User;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\DB;
 use \Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Validator;
 
 class TestController extends Controller
 {
     /**
-     * Get tests page info
+     * Display a listing of the test resource.
      *
-     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-	public function index(Request $request)
-	{
-		$query_params = $request->all();
-		
+    public function index(Request $request)
+    {
+        $query_params = $request->all();
+
 		$tests = [];
-		
-		if(isset($query_params['search'])){
-			$tests = DB::table('tests')->where('name', 'LIKE', '%' . $query_params['search'] . '%')
-						->get();
-		}else{
+
+		if (isset($query_params['search'])) {
+			$tests = DB::table('tests')->where(
+				'status',
+				'LIKE',
+				'%' . $query_params['search'] . '%'
+			)->get();
+		} else {
 			$tests = Test::all();
 		}
 
@@ -40,6 +41,101 @@ class TestController extends Controller
 				'data' => $tests
 			]
 		]);
-	}
+    }
 
+    /**
+     * Show the form for creating a new test resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return Inertia::render('Tests/Create');
+    }
+
+    /**
+     * Store a newly created test resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        $validationRules = [
+            'disease_id' => 'exists:App\Models\Disease,id',
+            'subject_id' => 'exists:App\Models\Subject,id',
+            'test_date' => 'required|date',
+            'status' => 'required|string|max:20'
+        ];
+
+        $validateData = Validator::make($data, $validationRules);
+
+        if ($validateData->fails()) {
+            return Redirect::route('tests.create')->withErrors($validateData);
+        }
+
+        Test::create($data);
+        return Redirect::route('tests')->with('success', 'Test successfully added.');
+    }
+
+    /**
+     * Show the form for editing the specified test resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $test = Test::find($id);
+
+        return Inertia::render('Tests/Edit', [
+            'test' => $test
+        ]);
+    }
+
+    /**
+     * Update the specified test resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data = $request->all();
+
+        $validationRules = [
+            'disease_id' => 'exists:App\Models\Disease,id',
+            'subject_id' => 'exists:App\Models\Subject,id',
+            'test_date' => 'required|date',
+            'status' => 'required|string|max:20'
+        ];
+
+        $validateData = Validator::make($data, $validationRules);
+
+        if ($validateData->fails()) {
+            return Redirect::route('tests.edit')->withErrors($validateData);
+        }
+
+        $tests = Test::find($id);
+        $tests->update($data);
+
+        return Redirect::route('tests')->with('success', 'Test successfully updated.');
+    }
+
+    /**
+     * Remove the specified test resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $test = Test::find($id);
+        $test->delete();
+
+        return Redirect::route('tests')->with('success', 'Test successfully deleted.');
+    }
 }
