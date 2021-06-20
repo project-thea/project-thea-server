e1fca310507d63418636f584aa66dd3decdcc121
Arnold Kunihira <arnold.kmulera@gmail.com>
2021-06-21 01:42:07 +0300

    Modify tracking controller and its view page

--- app/Http/Controllers/Web/TrackingController.php
+++ app/Http/Controllers/Web/TrackingController.php
@@ -3,19 +3,13 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
-use App\Models\Subject;
-use App\Models\Test;
-use App\Models\Disease;
-use App\Models\User;
 use Inertia\Inertia;
-use Illuminate\Support\Facades\DB;
 
 class TrackingController extends Controller
 {
 	public function index()
 	{
-		
-		return Inertia::render('Tracking', [
+		return Inertia::render('Tracking/Index', [
 			'data' => []
 		]);
 	}
