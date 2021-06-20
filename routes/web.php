81628ee0ddcf0557217e68e14b0fbe7bb7a3dabd
Arnold Kunihira <arnold.kmulera@gmail.com>
2021-06-21 02:24:15 +0300

    Remove deleted files and modify routes file to map resource routes

--- routes/web.php
+++ routes/web.php
@@ -21,53 +21,46 @@ use App\Http\Controllers\Web\TestController;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Auth/Login', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
+	return Inertia::render('Auth/Login', [
+		'canLogin' => Route::has('login'),
+		'canRegister' => Route::has('register'),
 		'canResetPassword' => Route::has('password.request'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+		'laravelVersion' => Application::VERSION,
+		'phpVersion' => PHP_VERSION,
+	]);
 });
 
-Route::middleware(['auth:sanctum', 'verified'])
-	->get('/dashboard', [DashboardController::class, 'index'])
-	->name('dashboard');
-
-Route::middleware(['auth:sanctum', 'verified'])
-	->get('/tests', [TestController::class, 'index'])
-	->name('tests');
-
-Route::middleware(['auth:sanctum', 'verified'])
-	->get('/subjects', [SubjectController::class, 'index'])
-	->name('subjects');
-
-//Route::middleware(['auth:sanctum', 'verified'])
-//	->get('/diseases', [DiseaseController::class, 'index'])
-//	->name('diseases');
-//
-//Route::middleware(['auth:sanctum', 'verified'])
-//	->get('/diseases/edit/{id}', [DiseaseController::class, 'edit'])
-//	->name('diseases.edit	');
-	
-	
-Route::middleware(['auth:sanctum', 'verified'])
-	->get('/tracking', [TrackingController::class, 'index'])
-	->name('tracking');
-
-Route::middleware(['auth:sanctum', 'verified'])
-	->get('/analysis', [AnalysisController::class, 'index'])
-	->name('analysis');
-	
-	
 Route::group(['middleware' => ['auth:sanctum', 'verified']], function () {
-    Route::get('/diseases', [DiseaseController::class, 'index'])->name('diseases');
-    Route::get('/diseases/edit/{id}', [DiseaseController::class, 'edit'])->name('diseases.edit');
-	Route::post('/diseases/update/{id}', [DiseaseController::class, 'update'])->name('diseases.update');
-	Route::post('/diseases/add', [DiseaseController::class, 'add'])->name('diseases.add');
-	Route::get('/diseases/new', function(){
-		return Inertia::render('Diseases/Add');
-	})->name('diseases.new');
-	Route::delete('diseases/trash/{disease}', [DiseaseController::class, 'destroy'])->name('diseases.destroy');
+	// Analysis
+	Route::get('/analysis', [AnalysisController::class, 'index'])->name('analysis');
 
+	// Tracking
+ 	Route::get('/tracking', [TrackingController::class, 'index'])->name('tracking');
+
+	// Dashboard
+	Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+	//	Subjects
+	Route::get('/subjects', [SubjectController::class, 'index'])->name('subjects');
+	Route::get('/subjects/create', [SubjectController::class, 'create'])->name('subjects.create');
+	Route::post('/subjects', [SubjectController::class, 'store'])->name('subjects.store');
+	Route::get('/subjects/{subject}/edit', [SubjectController::class, 'edit'])->name('subjects.edit');
+	Route::patch('/subjects/{subject}', [SubjectController::class, 'update'])->name('subjects.update');
+	Route::delete('/subjects/{subject}/trash', [SubjectController::class, 'destroy'])->name('subjects.destroy');
+
+	// Diseases
+	Route::get('/diseases', [DiseaseController::class, 'index'])->name('diseases');
+	Route::get('/diseases/create', [DiseaseController::class, 'create'])->name('diseases.create');
+	Route::post('/diseases', [DiseaseController::class, 'store'])->name('diseases.store');
+	Route::get('/diseases/{disease}/edit', [DiseaseController::class, 'edit'])->name('diseases.edit');
+	Route::patch('/diseases/{disease}', [DiseaseController::class, 'update'])->name('diseases.update');
+	Route::delete('/diseases/{disease}/trash', [DiseaseController::class, 'destroy'])->name('diseases.destroy');
+
+	// Tests
+	Route::get('/tests', [TestController::class, 'index'])->name('tests');
+	Route::get('/tests/create', [TestController::class, 'create'])->name('tests.create');
+	Route::post('/tests', [TestController::class, 'store'])->name('tests.store');
+	Route::get('/tests/{test}/edit', [TestController::class, 'edit'])->name('tests.edit');
+	Route::patch('/tests/{test}', [TestController::class, 'update'])->name('tests.update');
+	Route::delete('/tests/{test}/trash', [TestController::class, 'destroy'])->name('tests.destroy');
 });
