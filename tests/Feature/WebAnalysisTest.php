dfd41937d3639185a2d6ddba6cc529016e8b25f6
Arnold Kunihira <arnold.kmulera@gmail.com>
2021-06-21 02:43:43 +0300

    Modify web tests for analytics and tracking

--- tests/Feature/WebAnalysisTest.php
+++ tests/Feature/WebAnalysisTest.php
@@ -4,23 +4,18 @@ namespace Tests\Feature;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Str;
-use Laravel\Jetstream\Features;
 use Tests\TestCase;
-use Inertia\Controller;
-use Illuminate\Session\Middleware\StartSession;
-use Illuminate\Support\Facades\Route;
 use Inertia\Testing\Assert;
 use Laravel\Sanctum\Sanctum;
 
 class WebAnalysisTest extends TestCase
 {
     use RefreshDatabase;
-	
+
     public function test_can_view_analysis_page()
     {
-		$user = Sanctum::actingAs(User::factory()->create());
-		
+        $user = Sanctum::actingAs(User::factory()->create());
+
         $this->actingAs($user)
             ->get('/analysis')
             ->assertStatus(200)
