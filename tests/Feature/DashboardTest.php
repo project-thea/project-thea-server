be0deec3baa94f7305caba9d09cc5d26d3376115
Arnold Kunihira <arnold.kmulera@gmail.com>
2021-06-23 17:53:19 +0300

    Modify dashboard test

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -10,12 +10,17 @@ use Inertia\Testing\Assert;
 class DashboardTest extends TestCase
 {
     use RefreshDatabase;
-    
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = User::factory()->create();
+    }
+
     public function test_can_view_dashboard()
     {
-        $user = Sanctum::actingAs(User::factory()->create());
-
-        $this->actingAs($user)
+        $this->actingAs($this->user)
             ->get('/dashboard')
             ->assertStatus(200)
             ->assertInertia(function (Assert $page) {
