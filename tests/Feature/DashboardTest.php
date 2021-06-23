78e5b7a0e5f1804075f1aa8ea3c62666089116c4
Arnold Kunihira <arnold.kmulera@gmail.com>
2021-06-23 17:18:39 +0300

    Add dashboard test driven tests

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -4,34 +4,22 @@ namespace Tests\Feature;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Str;
-use Laravel\Jetstream\Features;
 use Tests\TestCase;
-use Inertia\Controller;
-use Illuminate\Session\Middleware\StartSession;
-use Illuminate\Support\Facades\Route;
 use Inertia\Testing\Assert;
 
 class DashboardTest extends TestCase
 {
     use RefreshDatabase;
-
-    public function setUp(): void
+    
+    public function test_can_view_dashboard()
     {
-        parent::setUp();
+        $user = Sanctum::actingAs(User::factory()->create());
 
-        $this->user = User::factory()->create();
-
-    }	
-
-	
-    //public function test_can_view_dashboard()
-    //{
-    //    $this->actingAs($this->user)
-    //        ->get('/dashboard')
-    //        ->assertStatus(200)
-    //        ->assertInertia(function (Assert $page) {
-    //            $page->component('Dashboard');
-    //        });
-    //}
+        $this->actingAs($user)
+            ->get('/dashboard')
+            ->assertStatus(200)
+            ->assertInertia(function (Assert $page) {
+                $page->component('Dashboard');
+            });
+    }
 }
