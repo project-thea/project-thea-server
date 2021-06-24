7db13c7d96c47fc9f2195d2743962264016f0a99
Arnold Kunihira <arnold.kmulera@gmail.com>
2021-06-24 19:01:24 +0300

    Modify tests

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -5,27 +5,22 @@ namespace Tests\Feature;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Inertia\Testing\Assert;
+use Laravel\Sanctum\Sanctum;
 
 class DashboardTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function setUp(): void
+    public function test_can_view_dashboard()
     {
-        parent::setUp();
+        $user = Sanctum::actingAs(User::factory()->create());
 
-        $this->user = User::factory()->create();
+        $this->actingAs($user)
+            ->get('/dashboard')
+            ->assertStatus(200)
+            ->assertInertia(function (Assert $page) {
+                $page->component('Dashboard');
+            });
     }
-
-    // public function test_can_view_dashboard()
-    // {
-    //     $user = Sanctum::actingAs(User::factory()->create());
-
-    //     $this->actingAs($user)
-    //         ->get('/dashboard')
-    //         ->assertStatus(200)
-    //         ->assertInertia(function (Assert $page) {
-    //             $page->component('Dashboard');
-    //         });
-    // }
 }
