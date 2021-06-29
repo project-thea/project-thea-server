7a4911709b0a805351ca9cfd43921b513b363da9
Arnold Kunihira <arnold.kmulera@gmail.com>
2021-06-29 13:11:32 +0300

    Add user controller to handle requests from user pages

---
+++ app/Http/Controllers/Web/UserController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers\WEB;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Redirect;
+use Inertia\Inertia;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the user resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = [];
+        $users = User::all();
+        $trashedUsers = User::onlyTrashed()->latest()->get();
+
+        return Inertia::render('Users/Index', [
+            'users' => [
+                'data' => $users
+            ],
+            'trashedUsers' => [
+                'data' => $trashedUsers
+            ]
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new user resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return Inertia::render('Users/Create');
+    }
+
+    /**
+     * Store a newly created user resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreUserRequest $request)
+    {
+        $validatedData = $request->validated();
+        User::create($validatedData);
+        return Redirect::route('manage.index')->with('success', 'User successfully added.');
+    }
+
+    /**
+     * Show the form for editing the specified user resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $user = User::find($id);
+        return Inertia::render('Users/Edit', [
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * Update the specified user resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        $validatedData = $request->validated();
+        $user->update($validatedData);
+        return Redirect::route('manage.index')->with('success', 'User successfully updated.');
+    }
+
+    /**
+     * Remove the specified user resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user = User::find($id);
+        $user->delete();
+        return Redirect::route('manage.index')->with('success', 'User successfully deleted.');
+    }
+
+    /**
+     * Restore the specified user resource from trash.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function restore($id)
+    {
+        $user = User::withTrashed()->find($id);
+        $user->restore();
+        return Redirect::route('manage.index')->with('success', 'User successfully restored.');
+    }
+}
