7d68cb5a969b2bfe61792c7d6d809d36757bb25e
Arnold Kunihira <arnold.kmulera@gmail.com>
2021-07-03 13:07:08 +0300

    Add dummy data in the respective table seeders

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -45,7 +45,7 @@ class UsersTableSeeder extends Seeder
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
                 'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
                 'role' => 'user'
-            ], 
+            ],
 
             [
                 'first_name' => 'Arnold',
@@ -55,6 +55,76 @@ class UsersTableSeeder extends Seeder
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
                 'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
                 'role' => 'admin'
+            ],
+
+            [
+                'first_name' => 'Chris',
+                'last_name' => 'Balya',
+                'email' => 'balya@gmail.com',
+                'password' => Hash::make('ba12345678'),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'role' => 'user'
+            ],
+
+            [
+                'first_name' => 'Joselyn',
+                'last_name' => 'Mudra',
+                'email' => 'mudra@gmail.com',
+                'password' => Hash::make('mu12345678'),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'role' => 'user'
+            ],
+
+            [
+                'first_name' => 'Doreen',
+                'last_name' => 'Muhangi',
+                'email' => 'dora@gmail.com',
+                'password' => Hash::make('do12345678'),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'role' => 'admin'
+            ],
+
+            [
+                'first_name' => 'Edwin',
+                'last_name' => 'Kiconco',
+                'email' => 'edy@gmail.com',
+                'password' => Hash::make('ed12345678'),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'role' => 'user'
+            ],
+
+            [
+                'first_name' => 'Oscar',
+                'last_name' => 'Musoke',
+                'email' => 'oscar@gmail.com',
+                'password' => Hash::make('os12345678'),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'role' => 'user'
+            ],
+
+            [
+                'first_name' => 'Joel',
+                'last_name' => 'Tibuhabz',
+                'email' => 'tibz@gmail.com',
+                'password' => Hash::make('tz12345678'),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'role' => 'user'
+            ],
+
+            [
+                'first_name' => 'Maxima',
+                'last_name' => 'Nsime',
+                'email' => 'max@gmail.com',
+                'password' => Hash::make('max12345678'),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'role' => 'user'
             ],
         ]);
     }
