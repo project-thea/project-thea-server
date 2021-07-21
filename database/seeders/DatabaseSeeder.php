e02c37d6151515bbcb36d71003931be87b774332
Arnold Kunihira <arnold.kmulera@gmail.com>
2021-07-21 22:40:59 +0300

    Modify users table seeder with role_id column and run all database seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            RolesTableSeeder::class,
             UsersTableSeeder::class,
             SubjectsTableSeeder::class,
             DiseasesTableSeeder::class,
