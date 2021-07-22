26a01df782cc3dee21203aed5251df39251039b7
Arnold Kunihira <arnold.kmulera@gmail.com>
2021-07-22 19:11:18 +0300

    Add validation to Fortify actions when creating new user

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -2,6 +2,7 @@
 
 namespace App\Actions\Fortify;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -20,12 +21,15 @@ class CreateNewUser implements CreatesNewUsers
      */
     public function create(array $input)
     {
+        $roles = Role::all();
+
         Validator::make($input, [
             'first_name' => ['required', 'string', 'max:255'],
             'last_name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => $this->passwordRules(),
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['required', 'accepted'] : '',
+            'role_id' => ['required']
         ])->validate();
 
         return User::create([
@@ -33,6 +37,7 @@ class CreateNewUser implements CreatesNewUsers
             'last_name' => $input['last_name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
+            'role_id' => $input['role_id']
         ]);
     }
 }
