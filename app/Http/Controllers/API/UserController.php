cc3c7a132e97529b1327bd5691167e77ff76e42f
Arnold Kunihira <arnold.kmulera@gmail.com>
2021-07-22 19:08:29 +0300

    Add role id column to API user controller

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -23,7 +23,8 @@ class UserController extends Controller
             'first_name' => 'required|string|max:55',
             'last_name' => 'required|string|max:55',
             'email' => 'email|required|string|unique:users',
-            'password' => 'required|string|confirmed'
+            'password' => 'required|string|confirmed',
+            'role_id' => 'required'
         ];
 
         $validateData = $request->validate($validationRules);
