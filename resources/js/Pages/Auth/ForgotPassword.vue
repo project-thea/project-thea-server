43edc36db4e236a8708968e994990bf4862d64e8
Arnold Kunihira <arnold.kmulera@gmail.com>
2021-07-22 18:05:21 +0300

    Add backend input validation

--- resources/js/Pages/Auth/ForgotPassword.vue
+++ resources/js/Pages/Auth/ForgotPassword.vue
@@ -17,7 +17,7 @@
         <form @submit.prevent="submit">
             <div>
                 <jet-label for="email" value="Email" />
-                <jet-input id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus />
+                <jet-input id="email" type="email" class="mt-1 block w-full" v-model="form.email" autofocus />
             </div>
 
             <div class="flex items-center justify-end mt-4">
