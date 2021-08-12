dc02d7492c8ec81783bdb003c632e137ef86cf08
Arnold Kunihira <arnold.kmulera@gmail.com>
2021-08-12 14:29:16 +0300

    Add questions model and its table schema

---
+++ database/migrations/2021_08_12_122839_create_questions_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('questionnaire_id')->constrained('questionnaires')->onDelete('cascade');
+            $table->foreignId('datatype_id')->constrained('data_types')->onDelete('cascade');
+            $table->string('title');
+            $table->json('attribute');
+            $table->string('position');
+            $table->integer('created_by')->nullable();
+            $table->integer('updated_by')->nullable();
+            $table->timestamps();
+
+            $table->index('title');
+            $table->index('position');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
