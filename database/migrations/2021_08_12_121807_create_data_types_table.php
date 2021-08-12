6ea6300124a7d062e909c088114255ed27536d94
Arnold Kunihira <arnold.kmulera@gmail.com>
2021-08-12 12:28:12 +0300

    Add datatypes model and its migration schema

---
+++ database/migrations/2021_08_12_121807_create_data_types_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDataTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->json('attribute');
+            $table->integer('created_by')->nullable();
+            $table->integer('updated_by')->nullable();
+            $table->timestamps();
+
+            $table->index('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('data_types');
+    }
+}
