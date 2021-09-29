a51ff3b137423c95a5410c7aacc2218dd2bb6a5f
Arnold Kunihira <arnold.kmulera@gmail.com>
2021-09-29 12:43:30 +0300

    Modify web routes file

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Web\DashboardController;
 use App\Http\Controllers\Web\TrackingController;
 use App\Http\Controllers\Web\AnalysisController;
 use App\Http\Controllers\Web\ProjectController;
+use App\Http\Controllers\Web\QuestionController;
 use App\Http\Controllers\Web\SubjectController;
 use App\Http\Controllers\Web\QuestionnaireController;
 use App\Http\Controllers\Web\UserController;
@@ -23,59 +24,65 @@ use App\Http\Controllers\Web\UserController;
 */
 
 Route::get('/', function () {
-	return Inertia::render('Auth/Login', [
-		'canLogin' => Route::has('login'),
-		'canRegister' => Route::has('register'),
-		'canResetPassword' => Route::has('password.request'),
-		'laravelVersion' => Application::VERSION,
-		'phpVersion' => PHP_VERSION,
-	]);
+    return Inertia::render('Auth/Login', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'canResetPassword' => Route::has('password.request'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
 Route::group(['middleware' => ['auth:sanctum', 'verified', 'throttle:web']], function () {
-	// Analysis
-	Route::get('/analysis', [AnalysisController::class, 'index'])->name('analysis');
+    // Analysis
+    Route::get('/analysis', [AnalysisController::class, 'index'])->name('analysis');
 
-	// Tracking
- 	Route::get('/tracking', [TrackingController::class, 'index'])->name('tracking');
+    // Tracking
+    Route::get('/tracking', [TrackingController::class, 'index'])->name('tracking');
 
-	// Dashboard
-	Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    // Dashboard
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-	//	Subjects
-	Route::get('/subjects', [SubjectController::class, 'index'])->name('subjects.index');
-	Route::get('/subjects/create', [SubjectController::class, 'create'])->name('subjects.create');
-	Route::post('/subjects', [SubjectController::class, 'store'])->name('subjects.store');
-	Route::get('/subjects/{subject}/edit', [SubjectController::class, 'edit'])->name('subjects.edit');
-	Route::patch('/subjects/{subject}', [SubjectController::class, 'update'])->name('subjects.update');
-	Route::delete('/subjects/{subject}/trash', [SubjectController::class, 'destroy'])->name('subjects.destroy');
-	Route::put('/subjects/{subject}/restore', [SubjectController::class, 'restore'])->name('subjects.restore');
+    //	Subjects
+    Route::get('/subjects', [SubjectController::class, 'index'])->name('subjects.index');
+    Route::get('/subjects/create', [SubjectController::class, 'create'])->name('subjects.create');
+    Route::post('/subjects', [SubjectController::class, 'store'])->name('subjects.store');
+    Route::get('/subjects/{subject}/edit', [SubjectController::class, 'edit'])->name('subjects.edit');
+    Route::patch('/subjects/{subject}', [SubjectController::class, 'update'])->name('subjects.update');
+    Route::delete('/subjects/{subject}/trash', [SubjectController::class, 'destroy'])->name('subjects.destroy');
+    Route::put('/subjects/{subject}/restore', [SubjectController::class, 'restore'])->name('subjects.restore');
 
-	// Projects
-	Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
-	Route::get('/projects/create', [ProjectController::class, 'create'])->name('projects.create');
-	Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
-	Route::get('/projects/{project}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
-	Route::patch('/projects/{project}', [ProjectController::class, 'update'])->name('projects.update');
-	Route::delete('/projects/{project}/trash', [ProjectController::class, 'destroy'])->name('projects.destroy');
-	Route::put('/projects/{project}/restore', [ProjectController::class, 'restore'])->name('projects.restore');
+    // Projects
+    Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
+    Route::get('/projects/create', [ProjectController::class, 'create'])->name('projects.create');
+    Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
+    Route::get('/projects/{project}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
+    Route::patch('/projects/{project}', [ProjectController::class, 'update'])->name('projects.update');
+    Route::delete('/projects/{project}/trash', [ProjectController::class, 'destroy'])->name('projects.destroy');
+    Route::put('/projects/{project}/restore', [ProjectController::class, 'restore'])->name('projects.restore');
 
-	// Questionnaires
-	Route::get('/questionnaires', [QuestionnaireController::class, 'index'])->name('questionnaires.index');
-	Route::get('/questionnaires/create', [QuestionnaireController::class, 'create'])->name('questionnaires.create');
-	Route::post('/questionnaires', [QuestionnaireController::class, 'store'])->name('questionnaires.store');
-	Route::get('/questionnaires/{questionnaire}/edit', [QuestionnaireController::class, 'edit'])->name('questionnaires.edit');
-	Route::patch('/questionnaires/{questionnaire}', [QuestionnaireController::class, 'update'])->name('questionnaires.update');
-	Route::delete('/questionnaires/{questionnaire}/trash', [QuestionnaireController::class, 'destroy'])->name('questionnaires.destroy');
-	Route::put('/questionnaires/{questionnaire}/restore', [QuestionnaireController::class, 'restore'])->name('questionnaires.restore');
+    // Questionnaires
+    Route::get('/questionnaires', [QuestionnaireController::class, 'index'])->name('questionnaires.index');
+    Route::get('/questionnaires/create', [QuestionnaireController::class, 'create'])->name('questionnaires.create');
+    Route::post('/questionnaires', [QuestionnaireController::class, 'store'])->name('questionnaires.store');
+    Route::get('/questionnaires/{questionnaire}/edit', [QuestionnaireController::class, 'edit'])->name('questionnaires.edit');
+    Route::patch('/questionnaires/{questionnaire}', [QuestionnaireController::class, 'update'])->name('questionnaires.update');
+    Route::delete('/questionnaires/{questionnaire}/trash', [QuestionnaireController::class, 'destroy'])->name('questionnaires.destroy');
+    Route::put('/questionnaires/{questionnaire}/restore', [QuestionnaireController::class, 'restore'])->name('questionnaires.restore');
 
-	//Manage users
-	Route::get('/users', [UserController::class, 'index'])->name('users.index');
-	Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-	Route::post('/users', [UserController::class, 'store'])->name('users.store');
-	Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
-	Route::patch('/users/{user}', [UserController::class, 'update'])->name('users.update');
-	Route::delete('/users/{user}/trash', [UserController::class, 'destroy'])->name('users.destroy');
-	Route::put('/users/{user}/restore', [UserController::class, 'restore'])->name('users.restore');
+    // Questions
+    Route::post('/questionnaires/{questionnaire}/questions', [QuestionController::class, 'store'])->name('questions.store');
+    Route::get('/questionnaires/{questionnaire}/questions/{question}/edit', [QuestionController::class, 'edit'])->name('questions.edit');
+    Route::patch('/questionnaires/{questionnaire}/questions/{question}', [QuestionController::class, 'update'])->name('questions.update');
+    Route::delete('/questionnaires/{questionnaire}/questions/{question}/trash', [QuestionController::class, 'destroy'])->name('questions.destroy');
+    Route::put('/questionnaires/{questionnaire}/questions/{question}/restore', [QuestionController::class, 'restore'])->name('questions.restore');
 
+    //Manage users
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
+    Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::patch('/users/{user}', [UserController::class, 'update'])->name('users.update');
+    Route::delete('/users/{user}/trash', [UserController::class, 'destroy'])->name('users.destroy');
+    Route::put('/users/{user}/restore', [UserController::class, 'restore'])->name('users.restore');
 });
