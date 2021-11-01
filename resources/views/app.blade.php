098a8b5d3805407c9cacb76aa0f9717091c381a5
Arnold Kunihira <arnold.kmulera@gmail.com>
2021-11-01 10:14:05 +0300

    Add favicon to project THEA

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,6 +13,9 @@
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
+        <!-- Favicon -->
+        <link rel="icon" href="{{ asset('favicon.ico') }}">
+
         <!-- Scripts -->
         @routes
         <script src="{{ mix('js/app.js') }}" defer></script>
